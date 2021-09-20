397c2d41bac369229778c5181ff7a917c8e4b0ab
Ranita Saha <37957073+ranitasahaa1230@users.noreply.github.com>
2021-09-20 17:31:00 +0530

    completed

---
+++ script.js
@@ -0,0 +1,31 @@
+const text=document.querySelector('textarea');
+const submitBtn=document.querySelector('#submit-btn')
+const outputEl=document.querySelector('#output')
+
+submitBtn.addEventListener('click', submitHandler);
+
+let getUrl="https://api.funtranslations.com/translate/minion.json";
+
+function getTranslation(text){
+
+    return getUrl + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("error occurred", error);
+    alert("Something wrong with server! Try again after some time!😕")
+}
+
+function submitHandler(){
+    let inputText=text.value;
+    
+    fetch(getTranslation(inputText))
+     .then(response=>response.json())
+    .then(hey=>{
+        var translatedText=hey.contents.translated;
+        outputEl.innerText=translatedText;
+    })
+     .catch(errorHandler);
+
+
+}
